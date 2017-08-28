3489fbce4e711aa8bf42f8868f478456067276f7
genysis22 <31072514+genysis22@users.noreply.github.com>
2017-08-27 22:33:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function summer (sumVal) {
   return function (toAdd) {
     if (!toAdd) throw new Error('trying to pull a fast one?')
     return sumVal + toAdd
-  }
+  
 
 
 module.exports = summer
